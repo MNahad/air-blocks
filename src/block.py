af41b5cb9a4b9480d67c9c6e97f9a32278034a2d
Mohammed Nawabuddin <MNahad@users.noreply.github.com>
2019-01-06 23:07:16 +0000

    Fixed block append issue

--- src/block.py
+++ src/block.py
@@ -6,7 +6,7 @@ blockchainPath = "assets/blockchain.json"
 
 class Block:
     def __init__(self, data, prevHash):
-        self.data = str(data)
+        self.data = data
         self.timestamp = float(time.time())
         self.prevHash = prevHash
 
@@ -31,11 +31,11 @@ class Block:
         newBlock = {
             "Timestamp": float(self.timestamp),
             "PrevHash": prevStr,
-            "Data": str(self.data),
+            "Data": self.data,
             "Hash": hashStr,
         }
 
-        chain.update(newBlock)
+        chain.append(newBlock)
 
         with open(blockchainPath, 'w') as theChain:
             json.dump(chain, theChain)
@@ -50,29 +50,31 @@ def fullBlockchain():
 def loadPrevHash():
     with open(blockchainPath) as theChain:
         chain = json.load(theChain)
-    num = []
-    for c in chain["Hash"]:
-        num.append(ord(c))
-    theHash = bytes(num)
+    hashNum = []
+    for c in chain[len(chain)-1]["Hash"]:
+        hashNum.append(ord(c))
+    theHash = bytes(hashNum)
     return theHash
 
 
 if __name__ == "__main__":
     GBOAC_gen_data = {
         "AC": "G-BOAC",
-        "Sign": "MN",
     }
     genesisHash = hashlib.sha256()
     genesisHash.update(b"0")
     GBOAC_gen = Block(GBOAC_gen_data, genesisHash.digest())
     GBOAC_gen.addToTheChain()
 
+    print("New chain for " + GBOAC_gen_data["AC"])
     pprint(fullBlockchain())
 
     GBOAC_new_data = {
         "ChkDate": float(time.time()) - 86400,
+        "Sign": "MN",
     }
     GBOAC_new = Block(GBOAC_new_data, loadPrevHash())
     GBOAC_new.addToTheChain()
 
+    print("Appended block for " + GBOAC_gen_data["AC"])
     pprint(fullBlockchain())
